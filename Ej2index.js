a2371ad0c066ffa4ac9b9668fc8a105eadd784c6
maribelarcos14 <maribel.arcos025@gmail.com>
2021-01-25 20:03:51 +0100

    initial commit

---
+++ Ej2index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const liber = {
+    day: 27,
+    month: 'mayo'
+};
+console.log('Mi cumpleaños es el ' + liber.day + ' de ' + liber.month);
+
+const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+console.log(meses[4]);
